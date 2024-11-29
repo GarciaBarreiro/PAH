dd9757ed609c25c8bedfbb1603e1b856c849d367
GarciaBarreiro <garcia.barreiro@proton.me>
2024-11-29 13:56:21 +0100

    L3 modified sbatch script

--- 3/sbatch.sh
+++ 3/sbatch.sh
@@ -14,8 +14,8 @@ for m in 8 16 32 64 128 256 512 1024 2048 4196; do
     for n in 2 4 8 16 32 64 128 256 512 1024; do
         for p in 8 16 32; do
             if (( $n * $p < 2048 )); then
-                srun $1 $m $n $p $3 > cuda.out # cuda
-                srun $2 $m $n $p    > seq.out  # seq
+                srun $1 $m $n $p $3 > cuda.txt # cuda
+                srun $2 $m $n $p    > seq.txt  # seq
                 diff cuda.out seq.out   # unless error, output should be empty
             fi
         done
