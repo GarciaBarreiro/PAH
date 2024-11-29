118dd1ad7bce8428c51e380ec2b2fe02fa6b0039
GarciaBarreiro <garcia.barreiro@proton.me>
2024-11-29 13:19:54 +0100

    L3 added time measurements to seq

--- 3/shuffle_seq.c
+++ 3/shuffle_seq.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 #ifndef DEBUG
     #define DEBUG 0
 #else
     #define DEBUG 1
 #endif
+
+#define INIT_TIME(prev, init) \
+    gettimeofday(&prev, NULL); \
+    gettimeofday(&init, NULL);
+
+// remove overhead created by call to gettimeofday
+#define GET_TIME(prev, init, final, res) \
+    gettimeofday(&final, NULL); \
+    res = (final.tv_sec-init.tv_sec+(final.tv_usec-init.tv_usec)/1.e6) - \
+          (init.tv_sec-prev.tv_sec+(init.tv_usec-prev.tv_usec)/1.e6);
 
 void _printMatrix(float *A, int m, int n, int p) {
     for (int i = 0; i < m; i++) {
@@ -40,6 +51,9 @@ float getMin(float *A, int n, int p, int i, int j) {
 }
 
 int main(int argc, char *argv[]) {
+    struct timeval t_prev, t_init, t_final;
+    double seq_t;
+
     if (argc < 4) {
         printf("Usage: %s M N P\n", argv[0]);
         return 1;
@@ -61,6 +75,7 @@ int main(int argc, char *argv[]) {
     printf("INPUT:\n");
     _printMatrix(X, M, N, P);
 
+    INIT_TIME(t_prev, t_init);
     // step 1
     for (int i = 0; i < M; i++) {
         for (int j = 0; j < N; j++) {
@@ -112,12 +127,17 @@ int main(int argc, char *argv[]) {
             }
         }
     }
+    GET_TIME(t_prev, t_init, t_final, seq_t);
 
     printf("OUTPUT:\n");
     _printMatrix(X, M, N, P);
 
     free(X);
     free(t);
+
+    FILE *fp = fopen((argc > 4) ? argv[4] : "seq_out.csv", "a");
+    fprintf(fp, "%d,%d,%d,%ld,%f,%d\n", M, N, P, N * P * sizeof(float), seq_t, N * P);
+    fclose(fp);
 
     return 0;
 }
